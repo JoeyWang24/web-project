29fde2953755cd8abd56d687c131c13f7dae2f63
why000123 <58807343+why000123@users.noreply.github.com>
2020-06-09 12:40:05 +0800

    Add files via upload

---
+++ Share/src/com/Share/Constant.java
@@ -0,0 +1,22 @@
+package com.Share;
+
+public class Constant {
+
+	public static final String DEFAULT_USER_IMG="Static/assets/images/default_avatar.png";
+
+	//显示分享默认页大小
+	public static final int SHARE_PAGE_SIZE = 5;
+
+	/**********显示某类型的分享************/
+	//显示默认的分享
+	public static final Integer SHARE_TYPE_DEFAULT = 0;
+	//显示点赞的分享
+	public static final Integer SHARE_TYPE_LIKE = 1;
+	//显示评论的分享
+	public static final Integer SHARE_TYPE_COMMENT = 2;
+	//显示收藏的分享
+	public static final Integer SHARE_TYPE_COLLECT = 3;
+	//显示点赞榜
+	public static final Integer SHARE_TYPE_LIKE_RANKING = 4;
+
+}
